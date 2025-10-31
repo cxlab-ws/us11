5e731115030c0a1ad59c74274efa4ee051f173f4
Ronen Riesenfeld <32473724+cx-ronen-riesenfeld@users.noreply.github.com>
2025-10-31 13:54:07 -0600

    Update AdminBooks_jsp.java

--- AdminBooks_jsp.java
+++ AdminBooks_jsp.java
@@ -2,7 +2,7 @@
 public class AdminBooks_jsp extends HttpJspBase {
 
 //
-// x222 modification
+// x222 modification1111
 //
 //   Filename: Common.jsp
 //   Generated with CodeCharge  v.1.2.0
@@ -781,4 +781,5 @@ if ( conn != null ) conn.close();
   }
 }
  */
+
 
