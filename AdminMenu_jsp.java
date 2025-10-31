6012aa4306cd2804bfa5f08b9e0c72bf3a9829a8
Ronen Riesenfeld <32473724+cx-ronen-riesenfeld@users.noreply.github.com>
2025-10-31 13:59:18 -0600

    Update AdminMenu_jsp.java

--- AdminMenu_jsp.java
+++ AdminMenu_jsp.java
@@ -7,7 +7,7 @@ public class AdminMenu_jsp extends HttpJspBase {
 
 
 //
-//   Filename: Common.jsp1111
+//   Filename: Common.jsp11112222
 //   Generated with CodeCharge  v.1.2.0
 //   JSP.ccp build 05/21/2001
 //
@@ -534,4 +534,5 @@ if ( conn != null ) conn.close();
     }
   }
 }
+
 
